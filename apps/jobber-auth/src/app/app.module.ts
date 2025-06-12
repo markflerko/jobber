055fb95b260eb3c997e7e6c5b74927705389bca4
markflerko <markflerko@gmail.com>
2025-06-12 13:33:13 +0300

    12. graphql & libs part-1

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,7 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
