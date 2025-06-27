10542c6c93fbc375288e65d0d11aecc05436e0f4
markflerko <markflerko@gmail.com>
2025-06-27 11:41:13 +0300

    20. jwt, device envs

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
