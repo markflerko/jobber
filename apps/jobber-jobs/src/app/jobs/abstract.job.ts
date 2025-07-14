0781cf650af5fc52197f29c1ac52a3315bb8b482
markflerko <markflerko@gmail.com>
2025-07-14 11:21:42 +0300

    39. abstract consumer

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,8 +1,7 @@
 import { PulsarClient } from '@jobber/pulsar';
-import { OnModuleDestroy } from '@nestjs/common';
 import { Producer } from 'pulsar-client';
 
-export abstract class AbstractJob<T> implements OnModuleDestroy {
+export abstract class AbstractJob<T> {
   private producer: Producer;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
@@ -12,9 +11,5 @@ export abstract class AbstractJob<T> implements OnModuleDestroy {
       this.producer = await this.pulsarClient.createProducer(job);
     }
     await this.producer.send({ data: Buffer.from(JSON.stringify(data)) });
-  }
-
-  async onModuleDestroy() {
-    await this.producer.close();
   }
 }
