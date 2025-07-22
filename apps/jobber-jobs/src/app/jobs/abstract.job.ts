a38f7206153949bc92bda7feb8f005b28ccc0e75
markflerko <markflerko@gmail.com>
2025-07-22 18:17:01 +0300

    43. job validation

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,15 +1,29 @@
 import { PulsarClient, serialize } from '@jobber/pulsar';
+import { BadRequestException } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 import { Producer } from 'pulsar-client';
 
-export abstract class AbstractJob<T> {
+export abstract class AbstractJob<T extends object> {
   private producer: Producer;
+  protected abstract messageClass: new () => T;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
 
   async execute(data: T, job: string) {
+    await this.validateData(data);
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(job);
     }
     await this.producer.send({ data: serialize(data) });
+  }
+
+  private async validateData(data: T) {
+    const errors = await validate(plainToInstance(this.messageClass, data));
+    if (errors.length) {
+      throw new BadRequestException(
+        `Job data is invalid ${JSON.stringify(errors)}`
+      );
+    }
   }
 }
