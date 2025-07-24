bef051893a9b42b94021a87a6009312c2b751cb9
markflerko <markflerko@gmail.com>
2025-07-24 11:43:11 +0300

    44. pulsar batch producer

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -15,15 +15,38 @@ export abstract class AbstractJob<T extends object> {
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(job);
     }
+    if (Array.isArray(data)) {
+      for (const message of data) {
+        await this.send(message);
+      }
+      return;
+    }
+    await this.send(data);
+  }
+
+  private async send(data: T) {
     await this.producer.send({ data: serialize(data) });
   }
 
-  private async validateData(data: T) {
-    const errors = await validate(plainToInstance(this.messageClass, data));
-    if (errors.length) {
-      throw new BadRequestException(
-        `Job data is invalid ${JSON.stringify(errors)}`
-      );
+  private async validateData(data: T | T[]) {
+    if (Array.isArray(data)) {
+      const instances = plainToInstance(this.messageClass, data);
+      for (const instance of instances) {
+        const errors = await validate(instance);
+        if (errors.length) {
+          throw new BadRequestException(
+            `Job data is invalid ${JSON.stringify(errors)}`
+          );
+        }
+      }
+    } else {
+      const instance = plainToInstance(this.messageClass, data);
+      const errors = await validate(instance);
+      if (errors.length) {
+        throw new BadRequestException(
+          `Job data is invalid ${JSON.stringify(errors)}`
+        );
+      }
     }
   }
 }
