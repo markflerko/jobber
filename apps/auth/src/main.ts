0effa97cc4652e45f38400ee8e23b805f6ee2d85
markflerko <markflerko@gmail.com>
2025-09-09 03:32:35 +0300

    69. auth grpc url

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,6 +5,7 @@ import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { AUTH_PACKAGE_NAME } from '@jobber/grpc';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -12,6 +13,7 @@ async function bootstrap() {
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
     options: {
+      url: app.get(ConfigService).getOrThrow('AUTH_GRPC_SERVICE_URL'),
       package: AUTH_PACKAGE_NAME,
       protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
     },
