2ed4926647fc161da00121ca2d8b7165de1a633b
markflerko <markflerko@gmail.com>
2025-09-17 17:44:51 +0300

    73. build with grpc url

--- apps/jobs/src/app/jobs.module.ts
+++ apps/jobs/src/app/jobs.module.ts
@@ -7,19 +7,24 @@ import { AUTH_PACKAGE_NAME } from '@jobber/grpc';
 import { FibonacciJob } from './jobs/fibonacci/fibonacci.job';
 import { JobsResolver } from './jobs.resolver';
 import { JobsService } from './jobs.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     DiscoveryModule,
     PulsarModule,
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: AUTH_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          package: AUTH_PACKAGE_NAME,
-          protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
-        },
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.getOrThrow('AUTH_GRPC_SERVICE_URL'),
+            package: AUTH_PACKAGE_NAME,
+            protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
