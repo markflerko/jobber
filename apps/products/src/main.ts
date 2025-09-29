866f791e2171bbda3589f92374f3bef97c69e667
markflerko <markflerko@gmail.com>
2025-09-30 01:14:55 +0300

    81. products grpc

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -3,13 +3,26 @@
  * This is only a minimal backend to get started.
  */
 
+import { Packages } from '@jobber/grpc';
 import { init } from '@jobber/nestjs';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   await init(app);
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: app.get(ConfigService).getOrThrow('PRODUCTS_GRPC_SERVICE_URL'),
+      package: Packages.PRODUCTS,
+      protoPath: join(__dirname, '../../libs/grpc/proto/products.proto'),
+    },
+  });
+  await app.startAllMicroservices();
 }
 
 bootstrap();
